2ff88799dbb9aea056e49f4f8f32e7fe4797943c
Magnus Kirø <magnuskiro@gmail.com>
2014-02-05 11:06:47 +0100

    adding aliases

--- setup.sh
+++ setup.sh
@@ -27,6 +27,12 @@ ln -s ~/simpleBarPC/.xinitrc ~/.xinitrc
 rm ~/.config/awesome
 ln -s ~/simpleBarPC/.config/awesome ~/.config/awesome
 
+# adding aliases that are useful. 
+echo '
+alias h="history"
+alias l="ls -lAhoF --color --show-control-chars"
+' >> ~/.bashrc
+
 # configure auto start of x and awesome
 echo '
 if [ $(pidof awesome|wc -w) -eq 0 ]; then
